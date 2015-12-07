dcfc374c01c282a247a941160b6dfb0cc26e3aba
ByeongSu Do <bsdo@naver.com>
2015-12-07 20:20:57 +0900

    lint

--- scripts/Containers/Main/WritePost.js
+++ scripts/Containers/Main/WritePost.js
@@ -1,50 +1,53 @@
 /**
  * Created by dobyeongsu on 2015. 10. 15..
  */
-import React, { Component } from 'react';
+import React from 'react';
 import Radium from 'radium';
 
 import connectToStores from 'alt/utils/connectToStores';
 import PostStore from '../../Stores/PostStore';
 import ClubStore from '../../Stores/ClubStore';
 
-import { Editor }           from '../../Components/index';
-import { editor as styles } from '../../Components/Style/style_editor'
+import {Editor} from '../../Components/index';
 
-class WritePost extends Component {
-  static getStores() {
-    return [PostStore, ClubStore];
-  }
-
-  static getPropsFromStores() {
-    return {
-      PostStore: PostStore.getState(),
-      ClubStore: ClubStore.getState()
-    }
-  }
-
-  constructor() {
-    super();
-  }
-
+let WritePost = React.createClass({
+  displayName: 'WritePost',
+  propTypes: {
+    ClubStore: React.PropTypes.shape.isRequired,
+    history: React.PropTypes.shape.isRequired
+  },
   componentWillReceiveProps(nextProps) {
     console.log('componentWillReceiveProps');
     if (nextProps.PostStore.writeSuccess) {
-      this.props.history.pushState(null, '/')
+      this.props.history.pushState(null, '/');
     }
-  }
+  },
 
   render() {
     return (
       <div>
         <Editor
           {...this.props}
-          ClubStore={this.props.ClubStore}
-        />
+          ClubStore={this.props.ClubStore} />
 
       </div>
-
-    )
+    );
   }
-}
-export default WritePost = connectToStores(Radium(WritePost));
+});
+
+WritePost = connectToStores({
+  getStores() {
+    // this will handle the listening/unlistening for you
+    return [PostStore, ClubStore];
+  },
+
+  getPropsFromStores() {
+    // this is the data that gets passed down as props
+    return {
+      PostStore: PostStore.getState(),
+      ClubStore: ClubStore.getState()
+    };
+  }
+}, Radium(WritePost));
+
+export default WritePost;
