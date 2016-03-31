8be72991cc158e90c4d458025b313b2208eecff1
bsdo64 <bsdo@naver.com>
2016-04-01 03:53:15 +0900

    import to ubuntu

--- bin/frontServer.js
+++ bin/frontServer.js
@@ -5,7 +5,7 @@ require('require-cache')();
 
 var time = new Date;
 
-require('babel-core/register');
+require('babel-register');
 require('../frontServer/server');
 
 console.log(((new Date).getTime() - time.getTime()) + 'ms');
